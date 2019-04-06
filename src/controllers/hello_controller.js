748a49d968422e1c732bd353854e9a8db06371ea
Franklin Ticona <franklinjosmell@gmail.com>
2019-04-06 01:03:43 -0500

    Hello, Stimulus

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -1,5 +1,21 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "name" ]
 
+  greet() {
+    // const element = this.nameTarget;
+    // const name = element.value
+    // console.log(`Hello, ${name}!`)
+
+    console.log(`Hello, ${this.name}`)
+  }
+
+  get name() {
+    return this.nameTarget.value
+  }
+
+  connect() {
+    console.log("Hello Stimulus!", this.element);
+  }
 }
